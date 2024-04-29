7d7fecca03cded94ff5c848a5acf793fdb7ac990
Miles <dm@iondrive.co>
2024-04-29 23:52:02 +1000

    More arg operations and tests for renames

--- yaml_surgeon.py
+++ yaml_surgeon.py
@@ -6,27 +6,40 @@ def main():
     parser = argparse.ArgumentParser(description='Process and modify a yaml file')
 
     parser.add_argument('--filePath', type=str, help='Path to the yaml file')
-    parser.add_argument('--name', type=str, help='Name of the node to select')
-    parser.add_argument('--childOf', type=str, help='Select only children of a node with this name')
-    parser.add_argument('--duplicate', type=str, help='Copies the selected node and its children, giving it this name')
+    parser.add_argument('--named', nargs='+', type=str, help='Name of the node to select')
+    parser.add_argument('--nameContains', nargs='+', type=str, help='Name of the node to select contains this')
+    parser.add_argument('--namedAtLevel', nargs='2', type=str, help='Name of the node to select and its level')
+    parser.add_argument('--withParents', nargs='+', type=str, help='Select only children of a node with this name')
+    parser.add_argument('--withParentAtLevel', nargs='2', type=str, help='Name of the node to select and its level')
     parser.add_argument('--rename', type=str, help='What to rename the selected to')
     parser.add_argument('--delete', action='store_true', help='Delete the selected nodes')
+    parser.add_argument('--duplicateAs', type=str, help='Copies the selected node and its children, giving it this name')
+    parser.add_argument('--insertSibling', type=str, help='Inserts a sibling node for the selected with this name')
 
     args = parser.parse_args()
     print(f"Opening {args.filePath}")
     with open(args.filePath, 'r') as file:
         text = file.read()
     operation = YamlOperation(text)
-    if args.name:
-        operation.named(args.name)
-    if args.childOf:
-        operation.with_parents(args.childOf)
-    if args.duplicate:
-        operation.duplicate_as(args.duplicate)
+    if args.named:
+        operation.named(args.named)
+    if args.nameContains:
+        operation.name_contains(args.nameContains)
+    if args.namedAtLevel:
+        operation.named_at_level(args.namedAtLevel[0], args.namedAtLevel[1])
+    if args.withParents:
+        operation.with_parents(args.withParents)
+    if args.withParentAtLevel:
+        operation.with_parent_at_level(args.withParentAtLevel[0], args.withParentAtLevel[1])
     if args.rename:
         operation.rename(args.rename)
-    elif args.delete:
+    if args.delete:
         operation.delete()
+    if args.duplicateAs:
+        operation.duplicate_as(args.duplicateAs)
+    if args.insertSibling:
+        operation.insert_sibling(args.insertSibling)
+
     lines = operation.execute()
     print("\n".join(lines))
 
--- yaml_surgeon/test/operation/test_renames.py
+++ yaml_surgeon/test/operation/test_renames.py
@@ -6,7 +6,7 @@ from yaml_surgeon.yaml_operation import YamlOperation
 
 class TestRenames(unittest.TestCase):
 
-    def test_node_rename(self):
+    def test_node_rename_key(self):
         yaml_content = """
             - parent1:
                 - srv-100:
@@ -23,6 +23,47 @@ class TestRenames(unittest.TestCase):
         for node in parsed_yaml:
             if node.name == 'renamed-srv-100':
                 self.assertIn('renamed-srv-100', node.name)
+
+    def test_rename_value(self):
+        yaml_content = """
+            - parent1:
+                - srv-100:
+                    fast: true
+                - srv-200"""
+        lexed_lines = scan_text(yaml_content)
+        parsed_yaml = parse_line_tokens(lexed_lines)
+        output_yaml = YamlOperation(parsed_yaml, lexed_lines).named('true').rename('false').execute()
+        output_yaml_string = "\n".join(output_yaml)
+        expected_yaml_content = """
+            - parent1:
+                - srv-100:
+                    fast: false
+                - srv-200"""
+        self.assertEqual(expected_yaml_content, output_yaml_string)
+
+    def test_rename_flow_sequence(self):
+        yaml_content = """
+            - spam:
+                - bacon: [egg, spam]"""
+        output_yaml = YamlOperation(yaml_content).named('egg').rename('ham').execute()
+        output_yaml_string = "\n".join(output_yaml)
+        expected_yaml_content = """
+            - spam:
+                - bacon: [ham, spam]"""
+        self.assertEqual(expected_yaml_content, output_yaml_string)
+
+    def test_rename_flow_mapping(self):
+        yaml_content = """
+            - sausage:
+                - bacon: {egg: spam} # Hello"""
+        lexed_lines = scan_text(yaml_content)
+        parsed_yaml = parse_line_tokens(lexed_lines)
+        output_yaml = YamlOperation(parsed_yaml, lexed_lines).named('egg').rename('ham').execute()
+        output_yaml_string = "\n".join(output_yaml)
+        expected_yaml_content = """
+            - sausage:
+                - bacon: {ham: spam} # Hello"""
+        self.assertEqual(expected_yaml_content, output_yaml_string)
 
 
 if __name__ == '__main__':
